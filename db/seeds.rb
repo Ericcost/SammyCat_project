ae5f3a2bff6d2dab1eec5b3e24eb05f260ca4e19
Ericcost <eric.costerousse@laposte.net>
2022-11-28 11:50:51 +0100

    Last seed modification

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+picture = "https://images.pexels.com/photos/320014/pexels-photo-320014.jpeg?auto=compress&cs=tinysrgb&w=400"
+
+20.times do 
+    Item.create!(
+        title: Faker::Lorem.sentence(word_count: 2),
+        description: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false),
+        price: Faker::Number.number(digits: 2),
+        image_url: picture
+        )
+end
