d0a817b6c91dd34b4affc1123c89025c06565715
BenjaminCharmes <benjamin.charmes@gmail.com>
2022-11-28 14:45:04 +0000

    Add show user

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,7 @@
+class UserController < ApplicationController
+
+  def show
+    @user = current_user
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :orders
   resources :carts
   devise_for :users
+  resources :user, only: [:show]
   resources :items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
