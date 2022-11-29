b42625790b85014ce5ec00556cf5215c0e150e7f
Ericcost <eric.costerousse@laposte.net>
2022-11-29 15:58:06 +0100

    Last modification of ActiveStorage functionnality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'avatars/create'
   resources :orders
   resources :carts
   devise_for :users
-  resources :user, only: [:show]
+  resources :user, only: [:show] do 
+    resources :avatars, only: [:create]
+  end
   resources :items
   resources :join_table_cart_items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
