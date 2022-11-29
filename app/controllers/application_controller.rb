4524c32ee81ab997782703ee3d72de2ea03e0255
Jiyan01 <jiyandag@gmail.com>
2022-11-29 11:36:02 +0100

    cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :set_render_cart
+  before_action :initialize_cart
+
+  def set_render_cart
+    @render_cart = true
+  end
+
+  def initialize_cart
+    @cart ||= Cart.find_by(id: session[:cart_id])
+    if current_user
+      if @cart.nil?
+        @cart= Cart.create(user_id: current_user.id)
+        session[:cart_id] = @cart.id
+      end
+    end
+   end
 end
